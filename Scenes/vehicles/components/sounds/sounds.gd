1f42a95fb2a216d6393bddd5db089a72608e550b
Wulthus <sickness2024@protonmail.com>
2024-06-08 23:43:34 +0200

    Vehicle sounds logic implemented. Brake sound file is missing (INVESTIGATE!).

--- Scenes/vehicles/components/sounds/sounds.gd
+++ Scenes/vehicles/components/sounds/sounds.gd
@@ -5,8 +5,9 @@ var audio_state = {
 	"play_running": false,
 	"revved_up": false,
 	"revved_down": false,
-	
 }
+
+#------------------------------------------------------------------------LOOPING SOUNDS
 
 func _process(_delta):
 	
@@ -15,10 +16,12 @@ func _process(_delta):
 	if (audio_state.play_idle == true && $engine_idle.playing == false):
 		$engine_revloop.stop();
 		$engine_idle.play();
-	elif (audio_state.play_running == true && $engine_revloop.playing == false):
-		print("I ran");
+	
+	if (audio_state.play_running == true && $engine_revloop.playing == false):
 		$engine_idle.stop();
 		$engine_revloop.play()
+		
+#------------------------------------------------------------------------GEARBOX SOUNDS
 
 func _on_gearbox_forward_gear():
 	if ($"../mechanics".mechanics_state.gear != "forward"):
@@ -29,26 +32,34 @@ func _on_gearbox_reverse_gear():
 	if ($"../mechanics".mechanics_state.gear != "reverse"):
 		$reverse_gear.play()
 
+#------------------------------------------------------------------------THROTTLE APPLIED/RELEASED
 
 func _on_engine_throtte_applied():
+	audio_state.play_running = true;
 	audio_state.play_idle = false;
-	if (audio_state.revved_up == false && $engine_revup.playing == false):
+	if (audio_state.revved_up == false):
 		$engine_revup.play()
+		$engine_revdown.stop()
 		audio_state.revved_up = true
 		audio_state.revved_down = false
-		
-func _on_engine_revup_finished():
-	audio_state.revved_up = true;
-	audio_state.play_running = true;
-
 
 func _on_engine_throttle_released():
 	audio_state.play_running = false;
-	if (audio_state.revved_down == false && $engine_revdown.playing == false):
-		$engine_revdown.play();
-		audio_state.revved_up = false;
-		audio_state.revved_down = true;
-
-func _on_engine_revdown_finished():
-	audio_state.revved_down = true;
 	audio_state.play_idle = true;
+	if (audio_state.revved_down == false):
+		$engine_revup.stop();
+		$engine_revdown.play();
+		audio_state.revved_down = true;
+		audio_state.revved_up = false;
+
+
+#------------------------------------------------------------------------BRAKES
+
+func _on_brakes_brakes_applied():
+	if ($brakes.playing == false):
+		$brakes.play();
+
+
+func _on_brakes_brakes_released():
+	if ($brakes.playing == true):
+		$brakes.stop();
